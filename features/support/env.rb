0277d93f53ead950627a53ae5b62744979bcc5dd
Kieran Cornwall <kierancornwall@Kierans-MacBook-Pro.local>
2015-12-18 16:21:11 +0000

    env.rb updated and file code commmented

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,24 +1,23 @@
-require 'bundler/setup'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
-require 'rspec/expectations'
 require 'site_prism'
 require 'active_support'
+
+# The below active support entry ensures that all files in the 'lib' foolder are autoloaded as part of a program run
+# information on ActiveSupport can be found here in regards to rails applications (although it provides a useful guide)
+# here is the link to the rubydoc information on the ActiveSupport::Dependencies - http://www.rubydoc.info/docs/rails/2.3.8/ActiveSupport/Dependencies
+
 
 ActiveSupport::Dependencies.autoload_paths << File.expand_path(File.join(Dir.pwd, 'lib'))
 
 Capybara.configure do |config|
-  config.run_server = false
-  config.ignore_hidden_elements = false
-  config.default_max_wait_time= 10
-  config.default_driver = :selenium_firefox
-  config.match = :prefer_exact
+  config.run_server = false #To ensure a Rack server does not start
+  config.ignore_hidden_elements = false #to ensure that all hidden elements on a page are recorded/available
+  config.default_max_wait_time= 10 #wait time for asynchronus processes to finsh
+  config.default_driver = :selenium_chrome #Default browser - input :selenium_chrome to use chrome browser as stated in the driver registration below
+  config.match = :prefer_exact #this setting is to ensure Capybara has specific matching rather than fuzzy logic
 end
 
-Capybara.register_driver :selenium_chrome do |app|
+Capybara.register_driver :selenium_chrome do |app| # Registration of chrome driver
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-Capybara.register_driver :selenium_firefox do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
